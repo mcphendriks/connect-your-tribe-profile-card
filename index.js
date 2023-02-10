eb5c1bf0c55612d38d50904a5b523b6c9b58e7a0
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2023-02-10 08:16:46 +0100

    implement HTML

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Importeer express uit de node_modules map
-import express from 'express'
+import express, { response } from 'express'
+
+const url ='https://whois.fdnd.nl/api/v1/member/mcphendriks'
+const data = await fetch(url).then((response) => response.json())
+
 
 // Maak een nieuwe express app aan
 const app = express()
@@ -14,7 +18,7 @@ app.use(express.static('public'))
 // Maak een route voor de index
 app.get('/', function (req, res) {
   // res.send('Hello World!')
-  res.render('index')
+  res.render('index', data)
 })
 
 // Stel het poortnummer in waar express op gaat luisteren
